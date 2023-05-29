235f92c559f4e53eded5fa4bc60b3cbf4c5ef1cb
Eric Dattore <eric@dattore.me>
2023-05-29 12:09:36 -1000

    Use fork of pre-commit-hooks

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.pre-commit-hooks = {
-    url = "github:cachix/pre-commit-hooks.nix";
+    url = "github:ELD/pre-commit-hooks.nix/add-all-features-clippy-opt";
     inputs = {
       nixpkgs.follows = "nixpkgs";
       flake-compat.follows = "flake-compat";
